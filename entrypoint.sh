8552061b03d4cc4accc9b7a321081e986b5d7019
Alexandru Cirstea <alexandru.cirstea01@gmail.com>
2025-04-20 14:30:08 +0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) Unpack the Minifab cert bundle into the organizations/ layout
 mkdir -p /opt/fabric/organizations
-tar -xzf /opt/fabric/certs.tgz \
+tar -xzf /opt/fabric/vars/certs.tgz \
     --strip-components=1 \
     -C /opt/fabric/organizations \
     keyfiles
 
-# 2) Launch your Spring Boot JAR
+mv /opt/fabric/vars/profiles /opt/fabric/profiles
+
 exec java -jar /app/app.jar
